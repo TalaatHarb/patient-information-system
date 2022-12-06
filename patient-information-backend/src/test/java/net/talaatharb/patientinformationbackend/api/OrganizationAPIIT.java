615df8e01dffc016453fbe94a63b2efb45aced3c
Mohamed Talaat Harb <mohamed.harb@cegedim.com>
2022-12-06 16:28:10 +0200

    complete first refactor cycle by refactoring the tests

--- patient-information-backend/src/test/java/net/talaatharb/patientinformationbackend/api/OrganizationAPIIT.java
+++ patient-information-backend/src/test/java/net/talaatharb/patientinformationbackend/api/OrganizationAPIIT.java
@@ -5,32 +5,15 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import net.talaatharb.patientinformationbackend.dtos.OrganizationDTO;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@ActiveProfiles(profiles = "test")
-@Tag("integration")
-class OrganizationAPIIT {
-
-	@Autowired
-	private MockMvc mockMVC;
-
-	@Autowired
-	private ObjectMapper objectMapper;
+class OrganizationAPIIT extends AbstractAPIIT{
 
 	@Test
 	void testCreateOrganization() throws JsonProcessingException, Exception {
