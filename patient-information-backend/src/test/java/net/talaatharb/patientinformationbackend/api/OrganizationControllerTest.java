5eb0e6a06f6f9848f6e5a62ae55af4c843caeacd
Mohamed Talaat Harb <mohamed.harb@cegedim.com>
2022-12-06 16:53:20 +0200

    extending first feature with validation - red test

---
+++ patient-information-backend/src/test/java/net/talaatharb/patientinformationbackend/api/OrganizationControllerTest.java
@@ -0,0 +1,35 @@
+package net.talaatharb.patientinformationbackend.api;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.springframework.web.server.ResponseStatusException;
+
+import net.talaatharb.patientinformationbackend.dtos.OrganizationDTO;
+
+class OrganizationControllerTest {
+
+	private OrganizationController organizationApi;
+	
+	@BeforeEach
+	void setup() {
+		organizationApi = new OrganizationController();
+	}
+
+	@Test
+	void testCreateOrganizationFailsIfTheOrgnaizationHasNoName() {
+		// Arrange
+		// Fill in the details for an organization with no Name
+		OrganizationDTO organizationToCreate = new OrganizationDTO();
+
+		// Act
+		// Call create organization
+		Executable executable = () -> organizationApi.createOrganization(organizationToCreate);
+
+		// Assert
+		// An exception should be thrown
+		assertThrows(ResponseStatusException.class, executable);
+	}
+}
