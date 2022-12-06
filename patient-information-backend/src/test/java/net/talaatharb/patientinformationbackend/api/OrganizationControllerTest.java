4a8aa0523a897a07f84f1bafd8582fbcc2386074
Mohamed Talaat Harb <mohamed.harb@cegedim.com>
2022-12-06 17:45:01 +0200

    added test to change the structure of the code to use a facade

--- patient-information-backend/src/test/java/net/talaatharb/patientinformationbackend/api/OrganizationControllerTest.java
+++ patient-information-backend/src/test/java/net/talaatharb/patientinformationbackend/api/OrganizationControllerTest.java
@@ -1,22 +1,26 @@
 package net.talaatharb.patientinformationbackend.api;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.function.Executable;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.server.ResponseStatusException;
-
 import net.talaatharb.patientinformationbackend.dtos.OrganizationDTO;
+import net.talaatharb.patientinformationbackend.facades.OrganizationFacade;
 
+@ExtendWith(MockitoExtension.class)
 class OrganizationControllerTest {
 
+	@InjectMocks
 	private OrganizationController organizationApi;
 	
-	@BeforeEach
-	void setup() {
-		organizationApi = new OrganizationController();
-	}
+	@Mock
+	private OrganizationFacade organizationFacade;
 
 	@Test
 	void testCreateOrganizationFailsIfTheOrgnaizationHasNoName() {
@@ -32,5 +36,21 @@ class OrganizationControllerTest {
 		// Assert
 		// An exception should be thrown
 		assertThrows(ResponseStatusException.class, executable);
+	}
+	
+	@Test
+	void testCreateOrganizationCallsCorrespondingFacadeMethod() {
+		// Arrange
+				// Fill in the details for an organization
+				OrganizationDTO organizationToCreate = new OrganizationDTO();
+				organizationToCreate.setName("Test Organization");
+
+				// Act
+				// Call create organization
+				organizationApi.createOrganization(organizationToCreate);
+				
+				// Assert
+				// Facade create organization will be called
+				verify(organizationFacade).createOrganization(organizationToCreate);
 	}
 }
