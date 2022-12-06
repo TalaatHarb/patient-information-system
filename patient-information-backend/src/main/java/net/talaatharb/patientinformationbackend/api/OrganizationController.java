606e3f38df010c25c308c1e153fd8a0e46ce1c47
Mohamed Talaat Harb <mohamed.harb@cegedim.com>
2022-12-06 16:15:33 +0200

    first refactor of implementation

--- patient-information-backend/src/main/java/net/talaatharb/patientinformationbackend/api/OrganizationController.java
+++ patient-information-backend/src/main/java/net/talaatharb/patientinformationbackend/api/OrganizationController.java
@@ -2,22 +2,15 @@ package net.talaatharb.patientinformationbackend.api;
 
 import java.util.UUID;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import net.talaatharb.patientinformationbackend.dtos.OrganizationDTO;
 
 @RestController
-@RequestMapping("/api/v1/organizations")
-public class OrganizationController {
+public class OrganizationController implements OrganizationAPIs{
 
-	@PostMapping("")
-	@ResponseStatus(HttpStatus.CREATED)
-	public OrganizationDTO createOrganization(@RequestBody OrganizationDTO organizationToCreate) {
+	@Override
+	public OrganizationDTO createOrganization(OrganizationDTO organizationToCreate) {
 		organizationToCreate.setId(UUID.randomUUID());
 		return organizationToCreate;
 	}
