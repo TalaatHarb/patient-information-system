62448e41f4ce59f9f6be1a6f4b2aea6ec72496a4
Mohamed Talaat Harb <mohamed.harb@cegedim.com>
2022-12-06 14:23:21 +0200

    creating backend project

---
+++ patient-information-backend/src/test/java/net/talaatharb/patientinformationbackend/PatientInformationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.talaatharb.patientinformationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientInformationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
